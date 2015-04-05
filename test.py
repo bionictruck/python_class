bebb5912c2350b0a0e663afb9db3c1e63c54e2b7
bionictruck <foofyjay@gmail.com>
2015-04-05 09:32:58 -0500

    Updated test.py

--- test.py
+++ test.py
@@ -1,17 +1,2 @@
-# Use the file name mbox-short.txt as the file name
-fname = raw_input("Enter file name: ")
-count = 0
-x = 0
-try:
-    fh = open(fname)
-except:
-    print fname, "is not a valid file."
-    exit()
-for line in fh:
-    line = line.rstrip()
-    if not line.startswith("X-DSPAM-Confidence:"):
-        continue
-    x = float(line[20:])+x
-    count = count + 1
-
-print "Average spam confidence:", x / count
+stuff = dict()
+print stuff.get('candy',-1)
